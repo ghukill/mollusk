d2792f94e21edd0b0980e7b1664fdb2b9d1c66f4
Graham Hukill <ghukill@gmail.com>
2025-03-23 11:28:01 -0400

    repository install instructions, pre main merge

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # mollusk
 Mollusk
 
+## Overview
+
+Mollusk is a CLI and python library (`mollusk`).  The CLI can be used to create new repositories, where then the `mollusk` library is used.  This is similar to other python projects, e.g. Django, where there are commands to initialize and manage applications but it's also a python library.  If there is not a term for this, there should be 😎. 
+
 ## Development
 
 ### Installation
@@ -29,6 +33,27 @@ uv run ruff check src/
 
 ## Repository
 
-### Installation
+### Create New Repository
 
-_Coming soon..._
+Create a directory for the new repository:
+```shell
+mkdir /tmp/my-mollusk-repo
+cd /tmp/my-mollusk-repo
+```
+
+Create a python virtual environment and install the `mollusk` library:
+```shell
+uv init --bare --python 3.13
+uv venv .venv
+uv add git+https://github.com/ghukill/mollusk
+```
+
+Initialize a new Mollusk repository:
+```shell
+uv run mollusk init
+```
+
+Edit settings file:
+```shell
+vim settings.py
+```
--- src/mollusk/cli.py
+++ src/mollusk/cli.py
@@ -13,7 +13,6 @@ from mollusk import logger
 @click.option("--verbose", "-v", is_flag=True, help="Enable verbose output")
 def main(*, verbose: bool) -> None:
     """Mollusk CLI tool."""
-    # configure logging for CLI usage
     if verbose:
         logger.setLevel(logging.DEBUG)
         logger.debug("Debug logging enabled")
@@ -32,15 +31,18 @@ def ping() -> None:
 def init(*, location: str) -> None:
     """Initialize a new Mollusk repository."""
     repository_path = Path(location).resolve()
-    # Create the directory if it doesn't exist
+
     if not repository_path.exists():
         logger.info(f"Creating directory: '{repository_path}'")
         repository_path.mkdir(parents=True, exist_ok=True)
+
     template_dir = files("mollusk.templates") / "new_repository"
     library_dir = files("mollusk")
+
     with as_file(template_dir) as src, as_file(library_dir) as lib:
         shutil.copytree(src, repository_path, dirs_exist_ok=True)
         shutil.copy(lib / "settings.py", repository_path)
+
     logger.info(f"New mollusk repository created at: '{repository_path}'")
 
 
